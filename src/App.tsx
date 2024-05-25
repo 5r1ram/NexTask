f0c089b02ee4ea0ee94b2dcbe55576b72ecaae5f
5r1ram <sriramiiit33@gmail.com>
2024-05-25 15:54:10 +0530

    changes made

--- src/App.tsx
+++ src/App.tsx
@@ -20,12 +20,9 @@ import { BrowserRouter, Route, Routes, Outlet } from "react-router-dom";
 import routerBindings, { NavigateToResource, CatchAllNavigate, UnsavedChangesNotifier, DocumentTitleHandler } from "@refinedev/react-router-v6";
 import { BlogPostList, BlogPostCreate, BlogPostEdit, BlogPostShow } from "./pages/blog-posts";
 import { CategoryList, CategoryCreate, CategoryEdit, CategoryShow } from "./pages/categories";
-import { ColorModeContextProvider } from "./contexts/color-mode";
-import { Header } from "./components/header";
 import { Login } from "./pages/login";
 import { Register } from "./pages/register";
 import { ForgotPassword } from "./pages/forgotPassword";
-import { authProvider } from "./authProvider";
 
 const API_URL = "https://api.nestjs-query.refine.dev/graphql";
 const WS_URL = "wss://api.nestjs-query.refine.dev/graphql";
@@ -33,26 +30,18 @@ const WS_URL = "wss://api.nestjs-query.refine.dev/graphql";
 const gqlClient = new GraphQLClient(API_URL);
 const wsClient = createClient({ url: WS_URL });
 
-
-
-
 function App() {
-    
-
-    
-    
     return (
         <BrowserRouter>
         <GitHubBanner />
         <RefineKbarProvider>
-            <ColorModeContextProvider>
 <AntdApp>
             <DevtoolsProvider>
                 <Refine dataProvider={dataProvider(gqlClient)}
 liveProvider={liveProvider(wsClient)}
 notificationProvider={useNotificationProvider}
 routerProvider={routerBindings}
-authProvider={authProvider} 
+// authProvider={} 
                         resources={[
                             {
                                 name: "blog_posts",
@@ -91,7 +80,7 @@ authProvider={authProvider}
                                     fallback={<CatchAllNavigate to="/login" />}
                                 >
                                         <ThemedLayoutV2
-                                            Header={() => <Header sticky />}
+                                            // Header={() => <Header sticky />}
                                             Sider={(props) => <ThemedSiderV2 {...props} fixed />}
                                         >
                                             <Outlet />
@@ -137,7 +126,6 @@ authProvider={authProvider}
             <DevtoolsPanel />
             </DevtoolsProvider>
             </AntdApp>
-</ColorModeContextProvider>
         </RefineKbarProvider>
         </BrowserRouter>
       );
