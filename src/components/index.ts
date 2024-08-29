89de6a7b4a62ae8d5eee030d0d7ea002c738f95d
5r1ram <sriramiiit33@gmail.com>
2024-08-28 21:12:55 -0400

    Updated top cards

--- src/components/index.ts
+++ src/components/index.ts
@@ -7,6 +7,8 @@ import LatestActivitiesSkeleton from "./skeleton/latest-activities";
 import ProjectCardSkeleton from "./skeleton/project-card";
 import UpcomingEventsSkeleton from "./skeleton/upcoming-events";
 
+import DashboardTotalCountCard from "./home/total-count-card";
+
 export { 
     UpcomingEvents, 
     DealsChart,
@@ -15,5 +17,7 @@ export {
     KanbanColumnSkeleton,
     LatestActivitiesSkeleton,
     ProjectCardSkeleton,
-    UpcomingEventsSkeleton
+    UpcomingEventsSkeleton,
+
+    DashboardTotalCountCard
 }
